1aaf1800d961124734441635ae11a122cb53c23b
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2017-03-08 14:36:10 +0000

    Rust library contains working tree_size implementation.

--- rust_tree/src/lib.rs
+++ rust_tree/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use] extern crate cpython;
-use cpython::*;
+//use cpython;
+use cpython::{Python, PyObject, PyResult, PyIterator, PyClone, ObjectProtocol};
 
 #[cfg(test)]
 mod tests {
@@ -8,82 +9,69 @@ mod tests {
     }
 }
 
-// Walks iterator-tree to get total length of elements and elements-of-elements.
-// This consumes the Iterator, so make sure to call obj.clone_ref() and pass the
-// new reference to this function.
-pub fn tree_size(token: Python, input: Result<PyIterator, PyErr>) -> Result<usize, PyErr> {
-    match input {
-        Err(E) => { Err(E) },
-        Ok(tree) => {
-            let tree_iter = tree.into_iter();
-            if let (0, _) = tree_iter.size_hint() {
-                return Ok(1)
-            }
-            let mut accum = 1;  // 1 for this node
-            for child in tree_iter {
-                match subtree_size(token.clone(), child) {
-                    Ok(num) => { accum += num },
-                    Err(E) => return Err(E),
+// Recursively iterate the child nodes of a tree. All nodes must be iterables.
+fn rs_tree_size(token: Python, input: PyIterator) -> PyResult<usize> {
+    let mut accum = 1;  // For this node
+    for child in input.into_iter() {
+        match child {
+            Ok(child_obj) => {
+                match child_obj.iter(token) {
+                    Ok(child_iter) => {
+                        match rs_tree_size(token, child_iter) {
+                            Ok(num) => { accum += num },
+                            Err(e) => return Err(e),
+                        }
+                    },
+                    Err(e) => return Err(e),
                 }
-            }
-            Ok(accum)
-        },
-    }
-}
-
-fn subtree_size(token: Python, x: Result<PyObject, PyErr>) -> Result<usize, PyErr> {
-    if let Ok(pyx) = x {
-        let x_cpy = pyx.clone_ref(token.clone());
-        let x_maybe_iter = PyIterator::from_object(token.clone(), x_cpy);
-        match x_maybe_iter {
-            Ok(x_iter) => tree_size(token.clone(), Ok(x_iter)),
-            Err(dce) => Err(PyErr::new::<exc::TypeError, String>(token.clone(), format!("Error inside subtree_size, possibly tree node was not an iterator? '{:?}'", dce))),
+            },
+            Err(e) => return Err(e),
         }
-    } else {
-        Err(x.err().expect("Should be an error here?"))
     }
+    Ok(accum)
 }
 
-// This compiles, but is not usable because the type it specifies is infinite.
-use std::iter::IntoIterator;
-pub fn inf_tree_size<T: IntoIterator<Item=T>>(tree: T) -> usize {
-    tree.into_iter().fold(1, |acc, child| acc + inf_tree_size(child))
-}
-
-/* Original Cython
-cimport cython
-import numpy as np
-cimport numpy as np
-
+/* From the below Cython.
+ * Clearly, it's a lot longer in rust. But, we get type safety, clear compileable
+ * code, and it might be faster.
 def tree_size(t):
     if len(t) == 0:
         return 1
     return sum(tree_size(child) for child in t) + 1
+*/
+pub fn py_tree_size(token: Python, input: &PyObject) -> PyResult<usize> {
+    let my_input_ref = input.clone_ref(token);
+    match my_input_ref.iter(token) {
+        Ok(x_iter) => rs_tree_size(token, x_iter),
+        Err(e) => Err(e),
+    }
+}
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
+py_module_initializer!(libtreefuncs, initlibtreefuncs, PyInit_libtreefuncs, |py, m| {
+    m.add(py, "__doc__", "Experimental Rust replacement for Cython code in mdr.")?;
+    m.add(py, "tree_size", py_fn!(py, py_tree_size(input: &PyObject)))?;
+    Ok(())
+});
+
+/* Original Cython
 def _simple_tree_match(t1, t2):
-
     if t1 is None or t2 is None:
         return 0
-
     if t1.tag != t2.tag:
         return 0
 
     m = np.zeros((len(t1) + 1, len(t2) + 1), np.int)
 
-    for i from 1 <= i < m.shape[0]:
-        for j from 1 <= j < m.shape[1]:
+    for i in range(1, m.shape[0]):
+        for j in range(1, m.shape[1]):
             m[i, j] = max(m[i, j - 1], m[i - 1, j], m[i - 1, j - 1] + _simple_tree_match(t1[i - 1], t2[j - 1]))
     return 1 + m[m.shape[0]-1, m.shape[1]-1]
+*/
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
+/*
 def _clustered_tree_match(t1, t2, c1, c2):
-
     if t1 is None or t2 is None:
         return 0.0
-
     if t1.tag != t2.tag:
         return 0.0
 
